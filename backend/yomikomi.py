94430f97ba23248621220ec6687a9de8f63bf899
StarterinUtopian <75227495+StarterinUtopian@users.noreply.github.com>
2022-10-18 21:17:58 +0900

    Add files via upload

---
+++ backend/yomikomi.py
@@ -0,0 +1,23 @@
+# command に先に ncdump　-h 'path/file'　で中身をチェックするでも可
+import netCDF4
+import sys
+import numpy as np
+# ファイルを開く
+infile="/Users/aorimbe/Downloads/B2022001_A1_WW00_chlora.nc"
+nc = netCDF4.Dataset(infile,'r')
+# ーーーーーーーーーーーーーーーーー
+
+#次元の情報を入手する
+print (nc.dimensions) 
+lon = 4320
+lat = 2160
+rgb = 3
+eightbitcolor = 256
+time = 1
+altitude = 1
+
+#データを読み込む
+data=nc.variables['chlor_a'][:]
+
+#出力
+print(data)
